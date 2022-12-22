577be26bbf1e143531f99e2df7534ab05c763f46
hanjjungwoo <eod0323@naver.com>
2022-12-22 20:07:37 +0900

    1222_update
    #가게 기능 완료 #가게정보 메뉴pick

--- ZzimGgong/src/main/webapp/hjw/script/power.js
+++ ZzimGgong/src/main/webapp/hjw/script/power.js
@@ -1,20 +1,15 @@
-var on_off=document.querySelector(".shop_option_power");
-
-// function click_power(){
-//     on_off.style.background = "#61188B"
-// }
-var option=document.querySelectorAll(".shop_option_a");
 function click_power() {
-    console.log(document.querySelector(".shop_option_power").innerText);
-    if(document.querySelector(".shop_option_power").innerText=="OFF"){
-        document.querySelector(".shop_option_power").style.background = "#9867b2"
-        document.querySelector(".shop_option_power").style.color = "#f2ebf5"
-        document.querySelector(".shop_option_power").innerText="ON"
+    var on_off=document.querySelector(".shop_option_power");
+    console.log(on_off.innerText);
+    if(on_off.innerText=="OFF"){
+        on_off.style.background = "#9867b2"
+        on_off.style.color = "#f2ebf5"
+        on_off.innerText="ON"
         document.querySelector("#option1").disabled=false;
         document.querySelector("#option2").disabled=false;
         document.querySelector("#option3").disabled=false;
     }else {
-        document.querySelector(".shop_option_power").innerText="OFF"
+        on_off.innerText="OFF"
         document.querySelector("#option1").disabled=true;
         document.querySelector("#option2").disabled=true;
         document.querySelector("#option3").disabled=true;
