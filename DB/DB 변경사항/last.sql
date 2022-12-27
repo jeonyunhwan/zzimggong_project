103ac084aaefcf4792748cfaca4d10b6862f6d63
YooMinWoo <alsn0527@naver.com>
2022-12-27 17:18:26 +0900

    Update last.sql

--- DB/DB 변경사항/last.sql
+++ DB/DB 변경사항/last.sql
@@ -1,3 +1,16 @@
+--DROP TABLE JJIM_SCH;
+--DROP TABLE policy;
+--DROP TABLE waiting;
+--DROP TABLE resPick;
+--DROP TABLE menu;
+--DROP TABLE jjim_review;
+--DROP TABLE reservation;
+--DROP TABLE res_user;
+--DROP TABLE access_Token;
+--DROP TABLE jjim_user;
+--DROP TABLE sns_user;
+--DROP TABLE restaurant;
+
 create table restaurant(--가게 테이블
     resNum varchar2(30) constraint restaurant_resNum_pk primary key,
     res_name varchar2(50),
@@ -15,6 +28,31 @@ create table restaurant(--가게 테이블
     wating NUMBER,
     reservation NUMBER
 );
+INSERT INTO restaurant values('123-45-67890','연돈','alsn99@naver.com','서울특별시 마포구 서교동','010-8890-5234',
+'img','11:00-22:00','화요일','15:00-16:00','단체석 완비/주차 이용가능','매장 전화문의가 많아 전화응대가 어렵습니다.',5000,
+to_date('20221120','YYYY-MM-DD'),1,1);
+
+INSERT INTO restaurant values('123-45-67895','런던베이글','abc123@naver.com','서울특별시 서대문구 신촌동','010-8890-5234',
+'img','10:00-23:00','수요일','15:00-16:00','단체석 완비/주차 이용가능/남녀 화장실 구분','서비스 많이 드릴게요 놀러오세요.',5000,
+to_date('20221020','YYYY-MM-DD'),1,1);
+
+INSERT INTO restaurant values('123-45-67894','도원','aaa1222@naver.com','서울특별시 마포구 동교동','010-8890-5234',
+'img','12:00-20:00','목요일','-','주차 이용가능/무선 인터넷','정말 맛있을걸요 일단 먹어보세요',5000,
+to_date('20211120','YYYY-MM-DD'),1,0);
+
+INSERT INTO restaurant values('123-45-67893','은희네 해장국','wejp33@naver.com','서울특별시 은평구 불광동','010-8890-5234',
+'img','11:00-22:00','목요일','-','단체석 완비/반려동물 입장가능','은희가 하는 해장국집입니다.',5000,
+to_date('20201120','YYYY-MM-DD'),1,0);
+
+INSERT INTO restaurant values('123-45-67892','참이맛 감자탕','adsfasdf@naver.com','서울특별시 금천구 시흥동','010-8890-5234',
+'img','09:00-22:00','월요일','-','유아 놀이방/유아용 의자','감자탕 하면 역시 참이맛',5000,
+to_date('20220527','YYYY-MM-DD'),0,1);
+
+INSERT INTO restaurant values('123-45-67891','동화가든','qwer12@naver.com','서울특별시 중구 광희동','010-8890-5234',
+'img','10:00-22:00','월요일','15:00-16:00','단체석 완비/주차 이용가능','강릉을 대표하는 동화가든이 서울에 왔다.',5000,
+to_date('20201219','YYYY-MM-DD'),0,1);
+
+SELECT * FROM restaurant;
 -- 회원테이블
 CREATE TABLE jjim_user
 ( -- 회원테이블
@@ -30,6 +68,10 @@ CREATE TABLE jjim_user
     idAuth number constraint jjim_user_user_idAuth_ck check(idAuth in(0,1,2))
 );
 INSERT into jjim_user VALUES('alsn0527@naver.com','1234',0,'유민우','010-9204-5515','용오로 82',0,0);
+INSERT into jjim_user VALUES('hds123@naver.com','1234',0,'허다솜','010-1234-5678','토금북로 24',0,0);
+INSERT into jjim_user VALUES('ljw123@naver.com','1234',0,'이지원','010-9204-1234','문래로 180',0,0);
+INSERT into jjim_user VALUES('hjw123@naver.com','1234',0,'한정우','010-9204-9999','백범로 790',0,0);
+INSERT into jjim_user VALUES('jyh123@naver.com','1234',0,'전윤환','010-9204-7777','토금중로 54',0,0);
 
 
 -- 액세스토큰 테이블
@@ -49,6 +91,13 @@ create table res_user(
     phone_Num varchar2(50),
     res_adress varchar2(100)
 );
+INSERT INTO res_user values('123-45-67890','1234','dbalsdn@naver.com','연돈','010-9204-5515','용오로 84');
+INSERT INTO res_user values('123-45-67895','1234','alsn0000@naver.com','런던베이글','010-1111-1111','용오로 84');
+INSERT INTO res_user values('123-45-67894','1234','alsn0001@naver.com','도원','010-2222-2222','토금북로 30');
+INSERT INTO res_user values('123-45-67893','1234','alsn0002@naver.com','은희네 해장국','010-3333-3333','토금북로 32');
+INSERT INTO res_user values('123-45-67892','1234','alsn0003@naver.com','참이맛 감자탕','010-4444-4444','토금북로 20');
+INSERT INTO res_user values('123-45-67891','1234','alsn0004@naver.com','동화가든','010-5555-5555','토금북로 22');
+
 --포장 테이블
 CREATE TABLE take_out(
    user_email varchar2(30) CONSTRAINT take_out_user_email_FK REFERENCES jjim_user(user_email),
@@ -56,14 +105,27 @@ CREATE TABLE take_out(
    take_out_time DATE,
    receipt_time DATE
 );
+
 -- 예약 테이블
 CREATE TABLE reservation(
    user_email varchar2(30) CONSTRAINT reservation_user_email_FK REFERENCES jjim_user(user_email),
    resNum varchar2(30) constraint reservation_resNum_fk references restaurant(resNum),
    reserve_apply_person NUMBER,
    reserve_start_time DATE,
-   reserve_request varchar2(100)
+   reserve_request varchar2(100),
+   -- 승인/거절 컬럼. 0이면 대기, 1이면 승인, 2면 대기
+   reserve_state number
 );
+DROP TABLE reservation;
+INSERT INTO RESERVATION values('alsn0527@naver.com','123-45-67890',4,
+to_date('2023-01-06 14:00','YYYY-MM-DD HH24:MI'),'아기 의자 준비해주세요',0);
+
+INSERT INTO RESERVATION values('hjw123@naver.com','123-45-67894',2,
+to_date('2023-01-07 18:00','YYYY-MM-DD HH24:MI'),'안 맵게 해주세요',0);
+
+INSERT INTO RESERVATION values('jyh123@naver.com','123-45-67893',3,
+to_date('2023-01-08 20:00','YYYY-MM-DD HH24:MI'),'견과류 알러지 있습니다. 빼주세요.',0);
+
 -- 찜리뷰테이블
 CREATE TABLE jjim_review(
    review_code varchar2(20) PRIMARY KEY,
@@ -74,22 +136,68 @@ CREATE TABLE jjim_review(
    review_content varchar2(100),
    review_img varchar2(100)
 );
+CREATE SEQUENCE review_code INCREMENT BY 1 START WITH 1 MINVALUE 1 MAXVALUE 9999;
+
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'alsn0527@naver.com','123-45-67895',
+5,to_date('20221223','YYYY-MM-DD'),'정말 맛집이네요~','img');
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'hds123@naver.com','123-45-67895',
+4,to_date('20221224','YYYY-MM-DD'),'바삭바삭하네요','img');
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'ljw123@naver.com','123-45-67895',
+5,to_date('20221225','YYYY-MM-DD'),'친구한테 추천할게요','img');
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'hjw123@naver.com','123-45-67895',
+5,to_date('20221226','YYYY-MM-DD'),'다음에 또 올게요','img');
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'jyh123@naver.com','123-45-67895',
+5,to_date('20221227','YYYY-MM-DD'),'느낌있네요 정말','img');
+
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'hds123@naver.com','123-45-67894',
+3,to_date('20221223','YYYY-MM-DD'),'좀 별론데요','img');
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'ljw123@naver.com','123-45-67894',
+3,to_date('20221224','YYYY-MM-DD'),'진짜 느낌 없는데..','img');
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'hjw123@naver.com','123-45-67894',
+1,to_date('20221225','YYYY-MM-DD'),'진짜 최악이야 최악!!','img');
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'jyh123@naver.com','123-45-67894',
+3,to_date('20221226','YYYY-MM-DD'),'기대 이하입니다','img');
+
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'ljw123@naver.com','123-45-67893',
+5,to_date('20221223','YYYY-MM-DD'),'정말 맛있네요','img');
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'hjw123@naver.com','123-45-67893',
+5,to_date('20221222','YYYY-MM-DD'),'진짜 느낌있어요 이 집은','img');
+INSERT INTO jjim_review values(REVIEW_CODE.nextval,'jyh123@naver.com','123-45-67893',
+5,to_date('20221221','YYYY-MM-DD'),'저희 엄마가 한 것보다 맛있네요','img');
+
+
 -- 메뉴테이블
 create table menu(
     resNum varchar2(30) constraint menu_resNum_fk references restaurant(resNum),
     mCategory varchar(30),
-    menuName varchar2(30),
-    price number,
+    menuName varchar2(1000),
+    price varchar2(1000),
     food_image varchar2(100)
 );
+INSERT INTO menu values('123-45-67890','일식','등심돈까스/안심돈까스/치즈돈까스','15,000/17,000/18,000','img');
+INSERT INTO menu values('123-45-67895','브런치식','아메리카노/카페라떼/플랫화이트','5,000/6,000/6,000','img');
+INSERT INTO menu values('123-45-67894','중식','짜장면/짬뽕/쟁반짜장','6,000/7,000/13,000','img');
+INSERT INTO menu values('123-45-67893','한식','해장국/내장탕/양무침','10,000/12,000/15,000','img');
+INSERT INTO menu values('123-45-67892','한식','감자탕(소)/감자탕(중)/감자탕(대)','22,000/26,000/30,000','img');
+INSERT INTO menu values('123-45-67891','한식','짬뽕순두부/초두부/모두부','12,000/10,000/10,000','img');
+
 -- 매장픽 테이블
 create table resPick(
     resNum varchar2(30) constraint resPick_resNum_fk references restaurant(resNum),
-    mood varchar2(20),
-    purpose varchar2(20),
-    food_type varchar2(20),
-    table_type varchar2(20)
+    mood varchar2(300),
+    purpose varchar2(300),
+    food_type varchar2(300),
+    table_type varchar2(300)
 );
+
+
+INSERT INTO resPick values('123-45-67890','깔끔한/신나는/전망좋은','친구랑 같이 가기 좋은','일식','홀테이블');
+INSERT INTO resPick values('123-45-67895','깔끔한','데이트하기 좋은/친구랑 같이 가기 좋은','브런치/카페','-');
+INSERT INTO resPick values('123-45-67894','깔끔한','회식하기 좋은','중식','홀테이블/단체석 테이블');
+INSERT INTO resPick values('123-45-67893','전통적인','한 잔 하기 좋은 집/낮 술이 가능한/친구랑 같이 가기 좋은/회식하기 좋은','한식','단체석구비');
+INSERT INTO resPick values('123-45-67892','전통적인','친구랑 같이 가기 좋은','한식','홀테이블');
+INSERT INTO resPick values('123-45-67891','전통적인','친구랑 같이 가기 좋은','한식','-');
+
 
 -- 줄서기 테이블
 create table waiting
@@ -102,6 +210,28 @@ create table waiting
     cancel char check(cancel in('F','T')),
     enter_check char check(enter_check in('F','T'))
 );
+
+INSERT INTO WAITING VALUES('123-45-67895','alsn0527@naver.com',
+to_date('2023-01-06 14:00','YYYY-MM-DD HH24:MI'),0,4,'F','F');
+INSERT INTO WAITING VALUES('123-45-67895','hds123@naver.com',
+to_date('2023-01-06 14:30','YYYY-MM-DD HH24:MI'),0,2,'F','F');
+INSERT INTO WAITING VALUES('123-45-67895','ljw123@naver.com',
+to_date('2023-01-06 15:00','YYYY-MM-DD HH24:MI'),0,3,'F','F');
+INSERT INTO WAITING VALUES('123-45-67895','hjw123@naver.com',
+to_date('2023-01-06 15:30','YYYY-MM-DD HH24:MI'),0,6,'F','F');
+INSERT INTO WAITING VALUES('123-45-67895','jyh123@naver.com',
+to_date('2023-01-06 16:00','YYYY-MM-DD HH24:MI'),0,4,'F','F');
+
+INSERT INTO WAITING VALUES('123-45-67890','ljw123@naver.com',
+to_date('2023-01-06 15:00','YYYY-MM-DD HH24:MI'),0,4,'F','F');
+INSERT INTO WAITING VALUES('123-45-67890','hjw123@naver.com',
+to_date('2023-01-06 15:30','YYYY-MM-DD HH24:MI'),0,3,'F','F');
+INSERT INTO WAITING VALUES('123-45-67890','jyh123@naver.com',
+to_date('2023-01-06 16:00','YYYY-MM-DD HH24:MI'),0,2,'F','F');
+
+
+
+
 -- 정책 일부로 pk 안넣음
 create table policy(
     refund varchar2(500)
@@ -113,21 +243,6 @@ CREATE TABLE jjim_sch(
 	search_word varchar2(100),
 	search_date date
 );
-
-DROP TABLE JJIM_SCH;
-DROP TABLE policy;
-DROP TABLE waiting;
-DROP TABLE resPick;
-DROP TABLE menu;
-DROP TABLE jjim_review;
-DROP TABLE reservation;
-DROP TABLE res_user;
-DROP TABLE access_Token;
-DROP TABLE jjim_user;
-DROP TABLE sns_user;
-DROP TABLE restaurant;
-
-
 
 INSERT INTO jjim_sch values('alsn0527@naver.com','연돈',sysdate);
 INSERT INTO jjim_sch values('alsn0527@naver.com','런던베이글',sysdate);
@@ -141,14 +256,14 @@ INSERT INTO jjim_sch values('alsn0527@naver.com','해운대',sysdate);
 INSERT INTO jjim_sch values('alsn0527@naver.com','성수',sysdate);
 INSERT INTO jjim_sch values('alsn0527@naver.com','인천',sysdate);
 
--- 인기검색어 최근 7일 10개
-SELECT SEARCH_WORD  FROM (
-SELECT SEARCH_WORD , count(SEARCH_WORD) cnt 
-FROM (SELECT * FROM JJIM_SCH WHERE (sysdate-SEARCH_DATE) <8 )
-GROUP BY SEARCH_WORD 
-ORDER BY cnt DESC)
-WHERE ROWNUM<=10;
 
-INSERT INTO restaurant values('123-45-67890','연돈','alsn0527@naver.com','용오로 82','010-9204-5515',
-'img','11:00-22:00','화요일','15:00-16:00','단체석 완비/주차 이용가능','매장 전화문의가 많아 전화응대가 어렵습니다.',5000,
-to_date('20221120','YYYY-MM-DD'),1,1);
+
+
+
+-- 인기검색어 최근 7일 10개
+-- SELECT SEARCH_WORD  FROM (
+-- SELECT SEARCH_WORD , count(SEARCH_WORD) cnt 
+-- FROM (SELECT * FROM JJIM_SCH WHERE (sysdate-SEARCH_DATE) <8 )
+-- GROUP BY SEARCH_WORD 
+-- ORDER BY cnt DESC)
+-- WHERE ROWNUM<=10;
